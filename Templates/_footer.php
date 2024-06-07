ca703eb7ce86f7fb26cf142460b43506d75a5e81
Alaa <111735652+Alkatlabe91@users.noreply.github.com>
2024-06-07 02:07:01 +0200

    Update _footer.php
    updated footer

--- Templates/_footer.php
+++ Templates/_footer.php
@@ -1,3 +1,3 @@
 <div class="footer container-fluid bg-dark text-light">
-    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="#"  rel="noopener noreferrer">@Alkatlabe</a></p>
+    <p class="text-center py-2 mb-0">Copyright © 2024 Designed by <a href="#"  rel="noopener noreferrer">@Alkatlabe</a></p>
 </div>
